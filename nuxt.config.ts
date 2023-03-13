d595e39486b0f6f22e508e13524a374a98387d37
YJack0000 <jackleagends@gmail.com>
2023-03-13 18:03:09 +0800

    add head title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,16 @@ export default defineNuxtConfig({
     prerender: {
       routes: ['/']
     }
+  },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1',
+      title: '東蜂科技 - 官網',
+      meta: [
+        { name: 'description', content: '東蜂科技是一間專注於網頁開發的公司，我們提供網頁開發、網頁設計、網頁維護、網頁優化、網頁擴充等服務。' },
+      ],
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.svg' }]
+    }
   }
 })

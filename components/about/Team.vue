04dcd45fd9d21148adae89a45ec00898e70c7dbf
YJack0000 <jackleagends@gmail.com>
2023-03-16 18:00:14 +0800

    chore: add Ian to Team

--- components/about/Team.vue
+++ components/about/Team.vue
@@ -6,7 +6,7 @@
         class="mx-auto"
         src="/img/about/YJack.jpg"
         title="YJack0000"
-        role="專案管理"
+        role="軟體工程師"
         description="YJack 擁有物聯網、微服務和前端開發的經驗。曾經於賴瑞科技設計結合影像視覺和訊號模擬的硬體遊戲助手，目前負責經營與聯繫東蜂工作室。"
       />
       <UiPersonCard
@@ -20,10 +20,16 @@
         class="mx-auto"
         src="/img/about/Brandon.jpg"
         title="Brandon Lu"
-        role="軟體工程師"
+        role="專案管理"
         description="商學院出身的 Brandon 大學時與資訊領域教授研究為雲原生和微服務，並且在許多官方的前後端開發、聊天機器人競賽中得獎。"
       />
-      <UiPersonCard class="mx-auto" />
+      <UiPersonCard
+        class="mx-auto"
+        src="/img/about/Ian.jpg"
+        title="Ian Chiu"
+        role="軟體工程師"
+        description="Ian 是一位持續關注並學習新興技術並積極參與開源社區的貢獻和活動的軟體工程師。他善於解決各種開發中的問題和挑戰，擅長開發高品質、易用性強的網頁應用程式。"
+      />
     </div>
   </div>
 </template>

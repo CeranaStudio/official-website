373e8f2d90158fbe63738f013054c6149a407560
YJack0000 <jackleagends@gmail.com>
2023-04-01 03:37:35 +0800

    feature: add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-simple-sitemap'],
   tailwindcss: {
     configPath: '~/config/tailwind.config.js'
+  },
+  sitemap: {
+    siteUrl: 'https://cerana.tech'
   },
   nitro: {
     // prerender: {
@@ -15,10 +18,18 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       title: '東蜂工作室 - 官網',
       meta: [
-        { name: 'description', content: '東蜂科技是一間專注於軟體開發的工作室，我們提供網站開發、網頁設計、服務架構、資料庫設計、行動應用程式開發等服務。' },
+        {
+          name: 'description',
+          content:
+            '東蜂工作室擁有豐富實作經驗，我們將根據您的需求和目標，建造出一個完全符合您期望的網站，讓您的網站與眾不同。'
+        },
         { name: 'og:title', content: '東蜂科技 - 官網' },
-        { name: 'og:description', content: '東蜂科技是一間專注於軟體開發的工作室，我們提供網站開發、網頁設計、服務架構、資料庫設計、行動應用程式開發等服務。' },
-        { name: 'og:image', content: '/og-image.png' },
+        {
+          name: 'og:description',
+          content:
+            '東蜂工作室擁有豐富實作經驗，我們將根據您的需求和目標，建造出一個完全符合您期望的網站，讓您的網站與眾不同。'
+        },
+        { name: 'og:image', content: '/og-image.png' }
       ],
       link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
     }

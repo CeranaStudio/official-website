9e1e73993f437a73a21e4f4f4027c9c13babe7c9
YJack0000 <49984479+YJack0000@users.noreply.github.com>
2023-08-14 05:04:09 +0000

    chore: change font family to mono

--- app.vue
+++ app.vue
@@ -4,6 +4,10 @@
   </NuxtLayout>
 </template>
 <style>
+:root {
+  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+}
+
 body {
   margin: 0;
   color: #333;

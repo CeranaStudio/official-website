1870d9dbd43812be93d5a812bd462d304a119b07
YJack0000 <jackleagends@gmail.com>
2023-12-10 17:25:15 +0800

    update: Team member

--- components/about/Team.vue
+++ components/about/Team.vue
@@ -1,35 +1,49 @@
 <template>
-  <div>
-    <h1 class="text-center text-4xl font-bold">團隊</h1>
-    <div class="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
-      <UiPersonCard
-        class="mx-auto"
-        src="/img/about/YJack.jpg"
-        title="YJack0000"
-        role="軟體工程師"
-        description="YJack 擁有物聯網、微服務和前端開發的經驗。曾經於賴瑞科技設計結合影像視覺和訊號模擬的硬體遊戲助手，目前負責經營與聯繫東蜂工作室。"
-      />
-      <UiPersonCard
-        class="mx-auto"
-        src="/img/about/Yan.png"
-        title="Yan Chiu"
-        role="服務架構"
-        description="Yan Chiu 擁有四年的後端開發的經驗，現為於陽明交大研究區塊鏈。曾參與設計區塊鏈錢包管理系統與供應鏈碳排放管理系統，目前正在與東蜂工作室合作提供在線服務。"
-      />
-      <UiPersonCard
-        class="mx-auto"
-        src="/img/about/Brandon.jpg"
-        title="Brandon Lu"
-        role="專案管理"
-        description="商學院出身的 Brandon 大學時與資訊領域教授研究為雲原生和微服務，並且在許多官方的前後端開發、聊天機器人競賽中得獎。"
-      />
-      <UiPersonCard
-        class="mx-auto"
-        src="/img/about/Ian.jpg"
-        title="Ian Chiu"
-        role="軟體工程師"
-        description="Ian 是一位持續關注並學習新興技術並積極參與開源社區的貢獻和活動的軟體工程師。他善於解決各種開發中的問題和挑戰，擅長開發高品質、易用性強的網頁應用程式。"
-      />
+    <div>
+        <h1 class="text-center text-4xl font-bold">團隊</h1>
+        <div class="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
+            <UiPersonCard
+                class="mx-auto"
+                src="/img/about/YJack.jpg"
+                title="YJack0000"
+                role="軟體工程師"
+                description="YJack 擁有豐富的物聯網、微服務和前端開發經驗。他曾在賴瑞科技負責設計一款結合影像視覺和訊號模擬的硬體遊戲助手。目前，他正負責經營並促進東蜂工作室的業務發展。"
+            />
+            <UiPersonCard
+                class="mx-auto"
+                src="/img/about/Yan.png"
+                title="Yan Chiu"
+                role="服務架構"
+                description="Yan Chiu 擁有四年後端開發經驗，目前在陽明交通大學研究區塊鏈技術。他曾參與設計區塊鏈錢包管理系統和供應鏈碳排放管理系統，現在正在與東蜂工作室合作開發在線服務。"
+            />
+            <UiPersonCard
+                class="mx-auto"
+                src="/img/about/Brandon.jpg"
+                title="Brandon Lu"
+                role="專案管理"
+                description="商學院畢業的 Brandon 在大學期間與資訊領域的教授共同研究雲原生和微服務。他在許多官方的前後端開發和聊天機器人競賽中屢獲殊榮。"
+            />
+            <UiPersonCard
+                class="mx-auto"
+                src="/img/about/Ian.jpg"
+                title="Ian Chiu"
+                role="軟體工程師"
+                description="Ian 是一名積極參與開源社區並持續關注新興技術的軟體工程師。他擅長解決開發過程中的各種問題和挑戰，並專注於開發高品質且易用的網頁應用程式。"
+            />
+            <UiPersonCard
+                class="mx-auto"
+                src="/img/about/CTHua.jpg"
+                title="CTHua"
+                role="軟體工程師"
+                description="CTHua 的目標是成為一名數位游牧者，他對探索各種技能領域充滿熱情。除了專業項目之外，他還涉獵物聯網、推薦系統、機器學習、量化金融等多個領域。"
+            />
+            <!-- <UiContextCard -->
+            <!--   class="mx-auto" -->
+            <!--   src="/img/about/Mark.jpg" -->
+            <!--   title="Mark" -->
+            <!--   role="...." -->
+            <!--   description="" -->
+            <!-- /> -->
+        </div>
     </div>
-  </div>
 </template>

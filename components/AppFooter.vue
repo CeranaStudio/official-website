c73363a137f67081552cc89d082f39d8429a86ec
YJack0000 <jackleagends@gmail.com>
2024-06-30 16:33:49 +0800

    feat: make foot better

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,10 @@
 <template>
     <footer class="my-10">
         <p class="text-center text-md text-slate-500">
-            © {{ new Date().getFullYear() }} Cerana Team. All rights reserved.
+            © {{ year }} Cerana Team. All rights reserved.
         </p>
     </footer>
 </template>
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
